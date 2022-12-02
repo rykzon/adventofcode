70375d283b8f434f8c9354c6261d8c6e88ab80f5
= <=>
2022-12-02 18:02:13 +0100

    0112

---
+++ 0112/0112.py
@@ -0,0 +1,40 @@
+#f = open("/home/rene/Documents/inventory.txt","r")
+#print(f.read())
+class Elve(object):
+    def __init__(self,name):
+        self.name = name
+        self.inventory = []
+    def __lt__(self,other):
+        return self.suminv() < other.suminv()
+    def __eq__(self,other):
+        return self.suminv() == other.suminv()
+    def add_inv(self,item):
+        self.inventory.append(item)
+    def suminv(self):
+        return sum(self.inventory)
+
+def make_elve(name):
+    elve = Elve(name)
+    return elve
+def get_carryElve(elves):
+    elves_sorted = sorted(elves,reverse=True)
+    return "Elve: "+elves_sorted[0].name + " Inventory: "+ str(elves_sorted[0].suminv())
+
+elves=[]
+elvecounter=1
+currentelve = make_elve("elve1")
+
+with open("/home/rene/Documents/inventory.txt") as f:
+    #lines = f.readlines()
+    lines = [line.rstrip() for line in f]
+
+for l in lines:
+    if l == "":
+        elvecounter=elvecounter+1
+        elves.append(currentelve)
+        currentelve = make_elve("elve"+str(elvecounter))
+    else:
+        currentelve.add_inv(int(l))
+elves.append(currentelve)
+
+print (get_carryElve(elves))
